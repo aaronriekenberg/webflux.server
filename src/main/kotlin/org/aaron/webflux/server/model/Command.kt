5afdd933a39f65fca31a419dee4ac27fefc59778
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2018-10-14 14:03:44 -0500

    Display command and arguments.

--- src/main/kotlin/org/aaron/webflux/server/model/Command.kt
+++ src/main/kotlin/org/aaron/webflux/server/model/Command.kt
@@ -7,11 +7,15 @@ data class MutableCommand(
         var arguments: MutableList<String>? = null) {
 
     fun toCommand(): Command {
+        val arguments: List<String> = this.arguments ?: emptyList()
+        val commandAndArguments = (listOf(this.command!!) + arguments).joinToString(separator = " ")
+
         return Command(
                 id = this.id!!,
                 description = this.description!!,
                 command = this.command!!,
-                arguments = this.arguments!!
+                arguments = arguments,
+                commandAndArguments = commandAndArguments
         )
     }
     
@@ -21,4 +25,5 @@ class Command(
         val id: String,
         val description: String,
         val command: String,
-        val arguments: List<String>)
+        val arguments: List<String>,
+        val commandAndArguments: String)
