40b98dea097587ecbe6a7e571e100944601f8a8e
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2018-10-14 15:00:18 -0500

    Add error handling.

--- src/main/kotlin/org/aaron/webflux/server/web/CommandPage.kt
+++ src/main/kotlin/org/aaron/webflux/server/web/CommandPage.kt
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.reactive.result.view.Rendering
+import reactor.core.publisher.Mono
 
 @Controller
 class CommandPage(
@@ -15,10 +16,16 @@ class CommandPage(
     companion object : KLogging()
 
     @GetMapping("/commands/{id}")
-    fun command(@PathVariable(required = true) id: String): Rendering {
-        return Rendering.view("command")
-                .modelAttribute("commandResult", commandService.runCommand(id))
-                .status(HttpStatus.OK)
-                .build()
+    fun command(@PathVariable(required = true) id: String): Mono<Rendering> {
+        return commandService.runCommand(id).map { commandResult ->
+            Rendering.view("command")
+                    .modelAttribute("commandResult", commandResult)
+                    .status(HttpStatus.OK)
+                    .build()
+        }.switchIfEmpty(Mono.just(
+                Rendering.view("error/404")
+                        .status(HttpStatus.NOT_FOUND)
+                        .build()
+        ))
     }
 }
