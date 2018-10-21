280af96d20561e73a47bac50469a2841a1af23c2
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2018-10-21 06:34:09 -0500

    Use runApplication.

--- src/main/kotlin/org/aaron/webflux/server/Application.kt
+++ src/main/kotlin/org/aaron/webflux/server/Application.kt
@@ -4,16 +4,14 @@ import org.aaron.webflux.server.config.CommandConfig
 import org.aaron.webflux.server.config.MainPageConfig
 import org.aaron.webflux.server.config.ProxyConfig
 import org.aaron.webflux.server.config.StaticPathsConfig
-import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.boot.runApplication
 
 @SpringBootApplication
 @EnableConfigurationProperties(CommandConfig::class, MainPageConfig::class, ProxyConfig::class, StaticPathsConfig::class)
-class Application {
-
-}
+class Application
 
 fun main(args: Array<String>) {
-    SpringApplication.run(Application::class.java, *args)
+    runApplication<Application>(*args)
 }
