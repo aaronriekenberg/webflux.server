652ce3223fe0e5136f671980d49bb6869bd7c415
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2018-10-26 06:19:31 -0500

    Add cache-control and last-modified to index.

--- src/main/kotlin/org/aaron/webflux/server/web/IndexPage.kt
+++ src/main/kotlin/org/aaron/webflux/server/web/IndexPage.kt
@@ -5,11 +5,14 @@ import org.aaron.webflux.server.config.MainPageConfig
 import org.aaron.webflux.server.service.CommandService
 import org.aaron.webflux.server.service.ProxyService
 import org.aaron.webflux.server.service.StaticPathsService
+import org.springframework.http.CacheControl
+import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.reactive.result.view.Rendering
 import java.time.OffsetDateTime
+import java.util.concurrent.TimeUnit
 
 @Controller
 class IndexPage(
@@ -20,16 +23,23 @@ class IndexPage(
 
     companion object : KLogging()
 
-    private val startTime = OffsetDateTime.now().toString()
+    private val startTime = OffsetDateTime.now()
+
+    private val startTimeString = startTime.toString()
 
     @GetMapping("/")
     fun index(): Rendering {
+        val headers = HttpHeaders().apply {
+            setCacheControl(CacheControl.maxAge(1, TimeUnit.MINUTES))
+            lastModified = startTime.toInstant().toEpochMilli()
+        }
         return Rendering.view("index")
                 .modelAttribute("mainPageConfig", mainPageConfig)
                 .modelAttribute("commands", commandService.getCommands())
                 .modelAttribute("proxies", proxyService.getProxies())
                 .modelAttribute("staticPaths", staticPathsService.getStaticPaths())
-                .modelAttribute("startTime", startTime)
+                .modelAttribute("startTime", startTimeString)
+                .headers(headers)
                 .status(HttpStatus.OK)
                 .build()
     }
